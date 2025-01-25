daa038c2a7ef21c9b852974816a7765abf029c46
iinsue <hinsu93@gmail.com>
2025-01-26 00:07:39 +0900

    Auth.js & Prisma & Supabase 연동

--- auth.ts
+++ auth.ts
@@ -1,8 +1,42 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "@/lib/db";
+import { getUserById } from "@/lib/user";
+import { getAccountByUserId } from "@/lib/account";
+
 import Google from "next-auth/providers/google";
 import Naver from "next-auth/providers/naver";
 import Kakao from "next-auth/providers/kakao";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
   providers: [Google, Naver, Kakao],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+
+      return token;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+      return session;
+    },
+  },
 });
